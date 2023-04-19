28eebb7e5dc03952d8d6a8c577105130d671b828
Paulo Henrique Pereira dos Santos <paulo.henriquepsantos@gmail.com>
2023-04-18 23:43:54 -0300

    correção do algoritmo

--- 1ºBimestre/OrdencaoInsercao/src/ordencaoinsercao/OrdencaoInsercao.java
+++ 1ºBimestre/OrdencaoInsercao/src/ordencaoinsercao/OrdencaoInsercao.java
@@ -15,12 +15,13 @@ public class OrdencaoInsercao {
         int[]vetor = {30,20,10,40};
         int chave, j;
         
-        for(int i = 0; i < vetor.length; i++){
+        for(int i = 1; i < vetor.length; i++){
             chave = vetor[i];
             
-            for(j = i - 1;  j >= 0 && vetor[j] > chave ; j-- ){
+            for(j = i - 1;  (j >= 0 && vetor[j] > chave) ; j-- ){
                 
                 vetor[j+1] = vetor[j];
+                
                 
             }
             vetor[j+1] = chave;
