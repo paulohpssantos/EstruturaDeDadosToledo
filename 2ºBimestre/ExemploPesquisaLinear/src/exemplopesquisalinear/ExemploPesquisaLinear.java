959fd99d92712044a215534f2db6a444a0a9c8bc
Paulo Henrique Pereira dos Santos <paulo.henriquepsantos@gmail.com>
2023-05-03 22:15:57 -0300

    fontes
    Atualizadio com pesquisa binaria

--- 2ºBimestre/ExemploPesquisaLinear/src/exemplopesquisalinear/ExemploPesquisaLinear.java
+++ 2ºBimestre/ExemploPesquisaLinear/src/exemplopesquisalinear/ExemploPesquisaLinear.java
@@ -22,6 +22,11 @@ public class ExemploPesquisaLinear {
                 "O elemento 40 está na posição: "+
                         pesquisaLinearRepetidos(numeroPesquisar, vetor2));
         
+        
+        JOptionPane.showMessageDialog(null, 
+                "O elemento 40 está na posição: "+
+                        pesquisaBinaria(numeroPesquisar, vetor));
+        
     }
     
     public static int pesquisaLinear(int numPesq, int[]vet){
@@ -39,10 +44,33 @@ public class ExemploPesquisaLinear {
         String posicoes = "";
         for (int i = 0; i < vet.length; i++) {
             if(numPesq == vet[i]){
-                posicoes += i+ ", ";
+                posicoes += i;
+                if(i < vet.length-1)
+                    posicoes += ", ";
             }
         }
         return posicoes;
+    }
+    
+    
+    public static int pesquisaBinaria(int numPesq, int[]vet){
+        int esquerda, meio, direita;
+        esquerda = 0;
+        direita = vet.length - 1;
+        
+        while(esquerda <= direita){
+            meio = (esquerda + direita)/2;
+            if(numPesq == vet[meio]){
+                return meio;
+            }
+            if(numPesq > vet[meio]){
+                esquerda = meio +1;
+            }else{
+                direita = meio -1;
+            }
+        }
+        
+        return -1;
     }
     
 }
